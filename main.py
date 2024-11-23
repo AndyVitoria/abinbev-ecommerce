135f9171c0c8970fe55463770ee0cff518dd38df
AndyVitoria <andre_davitoria@hotmail.com>
2024-11-23 13:49:48 -0300

    Adding client self registering route

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 import uvicorn
 
 from src.routes.health import health_router
+from src.routes.v1.clients import client_router
 
 
 app = FastAPI()
 
 app.include_router(health_router)
+app.include_router(client_router, prefix="/v1")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ src/routes/v1/clients/client.py
@@ -0,0 +1,18 @@
+from fastapi.routing import APIRouter, HTTPException
+
+from src.entities import ClientUser
+
+client_router = APIRouter(tags=["clients"], prefix="/clients")
+
+@client_router.post("/")
+def register_client(client_registry: ClientUser):
+    # Here you would typically add logic to save the user to a database
+    # For this example, we'll just print the user data and return a success message
+
+    try:
+        print(f"Registering Client: {client_registry.username}, Email: {client_registry.email}")
+        #user = ClientUser(username=client_registry.username, password=client_registry.password.get_secret_value(), email=client_registry.email)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    return {"message": "User registered successfully"}
+
