5020cbe7b7475d1cd4cccf3d5c2bf18a82391f66
AndyVitoria <andre_davitoria@hotmail.com>
2024-11-26 12:20:30 -0300

    Adding authentication route

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import uvicorn
 
 from src.routes.health import health_router
 from src.routes.v1.clients import client_router
+from src.routes.v1.auth import auth_router
 
 
 app = FastAPI()
 
 app.include_router(health_router)
 app.include_router(client_router, prefix="/v1")
+app.include_router(auth_router, prefix="/v1")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
