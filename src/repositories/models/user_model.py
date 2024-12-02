b43a16ed9363a1792575563230454b8a68d85899
AndyVitoria <andre_davitoria@hotmail.com>
2024-12-02 18:38:49 -0300

    Adding to_entity method to UserModel

--- src/repositories/models/user_model.py
+++ src/repositories/models/user_model.py
@@ -36,8 +36,26 @@ class UserModel(SQLModel, table=True):
             vault.get_secret("ACCESS_TOKEN_EXPIRE_MINUTES"),
         )
         return cls(
+            id=entity.id,
             username=entity.username,
             email=entity.email,
             password=crypt_utils.hash_password(entity.password.get_secret_value()),
             is_superuser=entity.is_superuser,
+        )
+
+    def to_entity(self):
+        """Creates an instance of User based on the UserModel provided
+
+        Returns
+        -------
+        User
+            The User created based on the UserModel provided
+        """
+        return User(
+            id=self.id,
+            username=self.username,
+            email=self.email,
+            password=self.password,
+            is_active=self.is_active,
+            is_superuser=self.is_superuser,
         )
