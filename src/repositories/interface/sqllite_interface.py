86bc311f2ed25dd41a5d8ce97b72d1d058d85b2c
AndyVitoria <andre_davitoria@hotmail.com>
2024-12-02 18:38:18 -0300

    Refactoring SQLLiteInterface

--- src/repositories/interface/sqllite_interface.py
+++ src/repositories/interface/sqllite_interface.py
@@ -17,7 +17,7 @@ class SQLLiteInterface:
         """
         self.database = SQLLiteDatabaseMonostate(database_url)
 
-    def create_or_update(self, model: SQLModel):
+    def create(self, model: SQLModel):
         """Creates or updates a model in the database
 
         Parameters
@@ -34,6 +34,35 @@ class SQLLiteInterface:
             session.add(model)
             session.commit()
         return model
+
+    def update(self, model: SQLModel):
+        """Creates or updates a model in the database
+
+        Parameters
+        ----------
+        model: SQLModel
+            The model to be created or updated
+
+        Returns
+        -------
+        SQLModel
+            The model created or updated
+        """
+        with Session(self.database.get_engine()) as session:
+            statement = select(model.__class__).where(model.__class__.id == model.id)
+            old_model = session.exec(statement).first()
+
+            if old_model:
+                for key, value in model.model_dump(exclude_unset=True).items():
+                    setattr(old_model, key, value)
+
+                session.add(old_model)
+                session.commit()
+                session.refresh(old_model)
+
+                return old_model
+            else:
+                raise ValueError("Model not found")
 
     def list(self, model_class: Type[SQLModel]):
         """Lists all models of a given class
@@ -70,3 +99,34 @@ class SQLLiteInterface:
         with Session(self.database.get_engine()) as session:
             statement = select(model_class).where(condition)
             return session.exec(statement).all()
+
+    def get_model_by_id(self, model_class: Type[SQLModel], model_id: int):
+        """Gets a model by its ID
+
+        Parameters
+        ----------
+        model_class: Type[SQLModel]
+            The class of the model to be retrieved
+        model_id: int
+            The ID of the model to be retrieved
+
+        Returns
+        -------
+        SQLModel
+            The model with the given ID
+        """
+        with Session(self.database.get_engine()) as session:
+            statement = select(model_class).where(model_class.id == model_id)
+            return session.exec(statement).first()
+
+    def delete(self, model: SQLModel):
+        """Deletes a model from the database
+
+        Parameters
+        ----------
+        model: SQLModel
+            The model to be deleted
+        """
+        with Session(self.database.get_engine()) as session:
+            session.delete(model)
+            session.commit()
