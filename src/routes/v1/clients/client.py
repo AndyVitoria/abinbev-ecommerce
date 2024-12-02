647a9bd3b425f233038421dbffb230f58b22f1bc
AndyVitoria <andre_davitoria@hotmail.com>
2024-12-02 18:36:35 -0300

    Refactoring client route code

--- src/routes/v1/clients/client.py
+++ src/routes/v1/clients/client.py
@@ -1,21 +1,14 @@
-from fastapi.routing import APIRouter, HTTPException
+from fastapi.routing import APIRouter
 
 from src.entities import User
 from src.usecases import RegisterUser, UserRole
-
 
 client_router = APIRouter(tags=["clients"], prefix="/clients")
 
 
 @client_router.post("/")
 def register_client(client_registry: User):
-    try:
-        register_client_usecase = RegisterUser(
-            user=client_registry, role=UserRole.CLIENT
-        )
+    register_client_use_case = RegisterUser(user=client_registry, role=UserRole.CLIENT)
 
-        register_client_usecase.register_user(client_registry)
-
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    register_client_use_case.register_user(client_registry)
     return {"message": "User registered successfully"}
