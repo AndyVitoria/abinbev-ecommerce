9953f667b271c347f9cebdbb517c3a70c13c00ff
AndyVitoria <andre_davitoria@hotmail.com>
2024-12-02 18:29:37 -0300

    Correcting the method args

--- src/entities/users/user_pattern.py
+++ src/entities/users/user_pattern.py
@@ -3,9 +3,9 @@ class UserPatterns:
     __PASSWORD_PATTERN: str = r"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$"
 
     @property
-    def USERNAME_PATTERN(cls) -> str:
-        return cls.__USERNAME_PATTERN
+    def USERNAME_PATTERN(self) -> str:
+        return self.__USERNAME_PATTERN
 
     @property
-    def PASSWORD_PATTERN(cls) -> str:
-        return cls.__PASSWORD_PATTERN
+    def PASSWORD_PATTERN(self) -> str:
+        return self.__PASSWORD_PATTERN
