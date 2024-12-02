032809a48720cb94632fdaa353e59df275d2ca98
AndyVitoria <andre_davitoria@hotmail.com>
2024-12-02 18:34:18 -0300

    Adding Authentication route

--- src/routes/v1/auth/auth.py
+++ src/routes/v1/auth/auth.py
@@ -1,6 +1,5 @@
 from typing import Annotated
-
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from src.entities import User
 from src.usecases.login import LoginUseCase
@@ -15,18 +14,6 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = User(
         username=form_data.username, password=form_data.password, email="faker@mail.com"
     )
-    try:
-        login_usecase = LoginUseCase(user=user)
-        access_token = login_usecase.generate_token()
-    except ValueError as e:
-        raise HTTPException(
-            status_code=400, detail=str(e), headers={"WWW-Authenticate": "Bearer"}
-        )
-    except Exception as e:
-        # Log the exception to the monitoring service
-        raise HTTPException(
-            status_code=500,
-            detail="Internal server error",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    login_use_case = LoginUseCase(user=user)
+    access_token = login_use_case.generate_token()
     return access_token
