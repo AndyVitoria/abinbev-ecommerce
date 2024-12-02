9ab0a790a57e8562e3a9eb7f224dfebf296b7c6d
AndyVitoria <andre_davitoria@hotmail.com>
2024-12-02 18:33:45 -0300

    Updating CryptUtils with decode and sessions managing methods

--- src/utils/hash.py
+++ src/utils/hash.py
@@ -22,11 +22,19 @@ class CryptUtils:
         to_encode = data.copy()
         expires_delta = timedelta(self.__ACCESS_TOKEN_EXPIRE_MINUTES)
         if expires_delta:
-            expire = datetime.now(timezone.utc) + expires_delta
+            expiration = datetime.now(timezone.utc) + expires_delta
         else:
-            expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-        to_encode.update({"exp": expire})
+            expiration = datetime.now(timezone.utc) + timedelta(minutes=15)
+        to_encode.update({"expiration": expiration.isoformat()})
         encoded_jwt = jwt.encode(
             to_encode, self.__SECRET_KEY, algorithm=self.__ALGORITHM
         )
         return encoded_jwt
+
+    def decode_access_token(self, token: str):
+        return jwt.decode(token, self.__SECRET_KEY, algorithms=[self.__ALGORITHM])
+
+    def is_token_expired(self, token: str):
+        decoded_token = self.decode_access_token(token)
+        expiration = datetime.fromisoformat(decoded_token["expiration"])
+        return expiration < datetime.now(timezone.utc)
